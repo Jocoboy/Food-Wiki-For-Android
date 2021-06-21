b4b55657923675802ece8fdf656a35cade483c82
Jocoboy <Jocoboy@outlook.com>
2021-06-21 19:37:04 +0800

    ui design for register page

---
+++ app/src/main/java/com/zstu/foodwiki/RegisterActivity.java
@@ -0,0 +1,27 @@
+package com.zstu.foodwiki;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class RegisterActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+
+        Button btn_goto_login = findViewById(R.id.btn_to_login);
+        btn_goto_login.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(RegisterActivity.this,LoginActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        });
+    }
+}
