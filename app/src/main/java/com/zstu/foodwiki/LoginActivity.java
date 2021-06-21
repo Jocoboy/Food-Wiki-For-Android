681581fe9bf23124b32940141dc4685599e6d3f4
unreal-engine-channel <unreal-engine-channel@outlook.com>
2021-06-21 19:37:04 +0800

    ui design for register page

---
+++ app/src/main/java/com/zstu/foodwiki/LoginActivity.java
@@ -0,0 +1,29 @@
+package com.zstu.foodwiki;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        Button btn_goto_register = findViewById(R.id.btn_to_register);
+        btn_goto_register.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(LoginActivity.this,RegisterActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        });
+    }
+
+
+}
